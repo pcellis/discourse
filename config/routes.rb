8f382ea917129814e1652e83cebb79be7c41f61d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2019-03-13 14:20:45 +0100

    FEATURE: imap/smtp topics and posts synchronization

--- config/routes.rb
+++ config/routes.rb
@@ -507,6 +507,7 @@ Discourse::Application.routes.draw do
       get 'mentionable'
       get 'messageable'
       get 'logs' => 'groups#histories'
+      get 'mailboxes'
 
       collection do
         get "check-name" => 'groups#check_name'
@@ -527,6 +528,7 @@ Discourse::Application.routes.draw do
           manage/members
           manage/membership
           manage/interaction
+          manage/email
           manage/logs
         }.each do |path|
           get path => 'groups#show'
